9a310706a8f6872d8462639ec347c1beb5d70c51
dytjq0123 <dytjq0123@gmail.com>
2022-05-17 22:54:58 +0900

    프로필 이미지 출력 방법 수정

--- src/main/java/com/clone/instagram/controller/UserController.java
+++ src/main/java/com/clone/instagram/controller/UserController.java
@@ -5,13 +5,10 @@ import com.clone.instagram.dto.user.UserProfileDto;
 import com.clone.instagram.dto.user.UserUpdateDto;
 import com.clone.instagram.entity.User;
 import com.clone.instagram.service.UserService;
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,18 +18,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
 
     /**
      * 로그인 후 나타나는 메인 화면
@@ -66,8 +62,16 @@ public class UserController {
     }
 
     @PostMapping("/user/update")
-    public String updateProfile(UserUpdateDto userUpdateDto, @RequestParam("profileImgUrl") MultipartFile profileImgUrl, PrincipalDetails principalDetails) throws IOException {
+    public String updateProfile(UserUpdateDto userUpdateDto, @RequestParam("profileImgUrl") MultipartFile profileImgUrl, @AuthenticationPrincipal PrincipalDetails principalDetails) throws IOException {
         userService.update(userUpdateDto, profileImgUrl, principalDetails);
+
+        User user = principalDetails.getUser();
+
+        // 세션 등록
+//        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(principalDetails, principalDetails.getUser().getPassword());
+//        Authentication authentication = authenticationManager.authenticate(token);
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         return "redirect:/user/profile/" + userUpdateDto.getId();
     }
 
@@ -75,29 +79,77 @@ public class UserController {
     private String profileUploadFolder;
 
     @GetMapping("/profile_img/{profileUrl}")
-    public ResponseEntity<byte[]> profileImg(@PathVariable("profileUrl") String profileUrl, HttpServletResponse response) throws IOException {
+    public void profileImg(@PathVariable("profileUrl") String profileUrl, HttpServletResponse response) throws IOException {
 
-        String file = profileUploadFolder + profileUrl;
+        String filePath = profileUploadFolder + profileUrl;
 
-        response.setHeader("Content-Disposition", "attachment; filename=\""+ profileUrl +"\"");
-        response.setContentType("application/octet-stream");
+        File file = new File(filePath);
+        if(!file.isFile()){
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.write("<script type='text/javascript'>alert('조회된 정보가 없습니다.'); self.close();</script>");
+            out.flush();
+            return;
+        }
 
-        ServletOutputStream sos = response.getOutputStream();
+        FileInputStream fis = null;
+        new FileInputStream(file);
 
-        File f = new File(file);
-        FileInputStream fis = new FileInputStream(f);
-        byte[] imageByteArray = IOUtils.toByteArray(fis);
-//        byte[] b = new byte[512];
-//        int len;
-//
-//        while ((len = fis.read(b)) != -1) {
-//            sos.write(b, 0, len);
-//        }
-//
-        sos.close();
-        fis.close();
-        return new ResponseEntity<byte[]>(imageByteArray, (HttpHeaders)response, HttpStatus.OK);
+        BufferedInputStream in = null;
+        ByteArrayOutputStream bStream = null;
+        try {
+            fis = new FileInputStream(file);
+            in = new BufferedInputStream(fis);
+            bStream = new ByteArrayOutputStream();
+            int imgByte;
+            while ((imgByte = in.read()) != -1) {
+                bStream.write(imgByte);
+            }
 
+            String type = "";
+            String ext = FilenameUtils.getExtension(file.getName());
+            if (ext != null && !"".equals(ext)) {
+                if ("jpg".equals(ext.toLowerCase())) {
+                    type = "image/jpeg";
+                } else {
+                    type = "image/" + ext.toLowerCase();
+                }
+
+            }
+
+            response.setHeader("Content-Type", type);
+            response.setContentLength(bStream.size());
+
+            bStream.writeTo(response.getOutputStream());
+
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bStream != null) {
+                try {
+                    bStream.close();
+                } catch (Exception est) {
+                    est.printStackTrace();
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception ei) {
+                    ei.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (Exception efis) {
+                    efis.printStackTrace();
+                }
+            }
+        }
 
     }
 }
